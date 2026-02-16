f0f7c075046f6d8af52b42e1e0246db0df132e79
mario <mario.salascast@gmail.com>
2026-02-16 11:58:11 -0700

    [Agrega] se actualizo formulario

--- parcial 1/Actividades-de-clase/Formularios/js/scrip.js
+++ parcial 1/Actividades-de-clase/Formularios/js/scrip.js
@@ -7,7 +7,25 @@ boton_guardar.addEventListener("click", (e) => {
 
     const usuario = new Usuario(nombre.value, apellido.value);
     console.log(usuario);
-});
+    
+})
+function cambiarNumero(event) {
+    console.log(event.target.value);
+    const numeroElementos = event.target.value;
+    const contenido= document.querySelector("#contenedor_correos");
+    contenido.innerHTML = "";
+    for (let i = 1; i <= event.target.value; i++) {
+        
+        //se agrega contenido usando la insercion de html por medio del innerHTMl,
+        //  que agregara todo lo esta dentro de HtmlAgregar
+        //este metodo reemplaza todo lo que esta dentro de la etiqueta
+        
+    const htmlAgregar = `<label for="correo- ${i}">Ingrese correo  ${i}</label>
+           <input type="email" name="correo- ${i}" id="correo- ${i}">
+           <br>`;
+            contenido.innerHTML += htmlAgregar;
+        }
+}
 
 
 class Usuario {
